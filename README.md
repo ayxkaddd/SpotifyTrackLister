a147fa4573d97207b77b3616cbc1460323438256
ayxkaddd <zxcvbqwert390@gmail.com>
2022-10-05 17:17:38 +0300

    nahh

---
+++ README.md
@@ -0,0 +1,2 @@
+# spotify_tracks
+takes all your added tracks and writes it to a file
---
+++ main.py
@@ -0,0 +1,66 @@
+from colorama import init, Fore
+
+init(autoreset=True)
+
+suckmymeme = """
+                 ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
+                  ⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
+                    ⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
+                   ⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
+               ⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    """
+
+
+print(Fore.CYAN + suckmymeme)
+
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+client_id = "client_id"
+client_secret = "client_secret"
+scope = "user-library-read"
+redirect_uri = "spotify_redirect_url"
+
+oauth = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, scope=scope, redirect_uri=redirect_uri))
+
+
+def get_tracks(offset):
+    begin = 0
+    tracks = oauth.current_user_saved_tracks(limit=20, offset=offset)
+    items = tracks.get("items")
+    tracks_file = open("tracks.txt", "a", encoding="utf-8")
+    for item in items:
+        begin += 1
+        track = item.get("track")
+        artist = track.get("artists")
+        track_name = track.get("name")
+        artist = artist[0]
+        artist_name = artist.get("name")
+
+        tracks_string = f"{artist_name} - {track_name}\n"
+        tracks_file.write(tracks_string)
+        print(f"[{begin}] - {artist_name} - {track_name} to tracks.txt")
+        end = begin
+
+    print(Fore.GREEN + f"\t\t\t\t\twrited {end} tracks")
+    tracks_file.close()
+
+
+def main():
+    offset = -20
+    limit = 20
+    for i in range(count_of_added_tracks): # count of added tracks / 20
+        offset += limit
+        get_tracks(offset)
+
+
+if __name__ == "__main__":
+    main()
