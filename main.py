721949bf6db982aa022490803b44ddf860391ee5
мама я программист <82116858+ayxkaddd@users.noreply.github.com>
2022-10-06 18:11:20 +0300

    im bored

--- main.py
+++ main.py
@@ -21,13 +21,10 @@ suckmymeme = """
 
 print(Fore.CYAN + suckmymeme)
 
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
-
-oauth = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config.client_id, client_secret=config.client_secret, scope=scope, redirect_uri=config.redirect_uri))
+oauth = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=config.client_id, client_secret=config.client_secret, scope="user-library-read", redirect_uri=config.redirect_uri))
 
 
 def get_tracks(offset):
@@ -38,29 +35,30 @@ def get_tracks(offset):
     for item in items:
         begin += 1
         track = item.get("track")
-        artist = track.get("artists")
         track_name = track.get("name")
+        artist = track.get("artists")
         artist = artist[0]
         artist_name = artist.get("name")
 
         tracks_string = f"{artist_name} - {track_name}\n"
         tracks_file.write(tracks_string)
-        print(f"[{begin}] - {artist_name} - {track_name} [into] - {config.file_name}")
+        print(Fore.CYAN + f"[{begin}] {artist_name} - {track_name}")
         end = begin
-
-    print(Fore.GREEN + f"\t\t\t\t\twrited {end} tracks")
+    config.tracks_count += end
+    print(Fore.GREEN + f"\t\t\t\t\t\t\t\t\twrited {end} tracks")
     tracks_file.close()
 
 
 def main():
     offset = -20
     limit = 20
-    count_of_added_tracks = 439
-    for i in range(round(count_of_added_tracks / 20)): # count of added tracks / 20
+    count_of_added_tracks = your_number
+    for i in range(round(count_of_added_tracks / 20) + 1):
         offset += limit
         get_tracks(offset)
-    print(f"Done\n{config.file_name} filled")
+    print(Fore.GREEN + f"\n\t\t\t\t\tDone\n\t\t\t\t\t{config.file_name} filled\n\t\t\t\t\tTotal tracks count - {config.tracks_count}")
 
 
 if __name__ == "__main__":
+    print("///its uglu asf but idc")
     main()
